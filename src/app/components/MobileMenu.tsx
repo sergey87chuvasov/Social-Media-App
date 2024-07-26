40d4349b7c1a01c2eb16307508e119542f8b64fb
Sergey Chv <serge.techno@gmail.com>
2024-07-26 19:22:24 +0300

    added Responsive Navbar with Tailwind

---
+++ src/app/components/MobileMenu.tsx
@@ -0,0 +1,13 @@
+const MobileMenu = () => {
+    return (
+      <div className=''>
+        <div className="flex flex-col gap-[4.5px] cursor-pointer">
+            <div className="w-6 h-1 bg-blue-500 rounded-sm" />
+            <div className="w-6 h-1 bg-blue-500 rounded-sm" />
+            <div className="w-6 h-1 bg-blue-500 rounded-sm" />
+        </div>
+      </div>
+    )
+  }
+  
+  export default MobileMenu
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,6 +1,17 @@
+import Link from "next/link"
+import MobileMenu from "./MobileMenu"
+
 const Navbar = () => {
     return (
-      <div className=''>Navbar</div>
+      <div className='h-24 flex items-center justify-between'>
+        <div className="">
+            <Link href='/' className="font-bold text-xl text-blue-600">LAMASCOCIAL</Link>
+        </div>
+        <div className="hidden"></div>
+        <div className="">
+            <MobileMenu />
+        </div>
+      </div>
     )
   }
   
