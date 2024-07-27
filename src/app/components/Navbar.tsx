ea489b1a0639238efb3c7ed56aa134ec1272ddd6
Sergey Chv <serge.techno@gmail.com>
2024-07-27 18:40:28 +0300

    added Mobile First Design with Tailwind CSS

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,14 +1,30 @@
 import Link from "next/link"
 import MobileMenu from "./MobileMenu"
+import Image from "next/image"
 
 const Navbar = () => {
     return (
       <div className='h-24 flex items-center justify-between'>
-        <div className="">
+        <div className="md:hidden lg:block w-[20%]">
             <Link href='/' className="font-bold text-xl text-blue-600">LAMASCOCIAL</Link>
         </div>
-        <div className="hidden"></div>
-        <div className="">
+        <div className="hidden md:flex w-[50%] text-sm">
+            <div className="flex gap-6 text-gray-600">
+                <Link href='/' className="flex items-center gap-2">
+                    <Image src='/home.png' alt='homepage' width={16} height={16} className="w-4 h-4"/>
+                    <span>HomePage</span>
+                </Link>
+                <Link href='/' className="flex items-center gap-2">
+                    <Image src='/friends.png' alt='friends' width={16} height={16} className="w-4 h-4" />
+                    <span>Friends</span>
+                </Link>
+                <Link href='/' className="flex items-center gap-2">
+                    <Image src='/stories.png' alt='stories' width={16} height={16} className="w-4 h-4" />
+                    <span>Stories</span>
+                </Link>
+            </div>
+        </div>
+        <div className="w-[30%] flex items-center gap-4 xl:gap-8 justify-end">
             <MobileMenu />
         </div>
       </div>
