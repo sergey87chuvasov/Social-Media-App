a4fa593ec68b134eb76f678ba6c39b045ff93d04
Sergey87 <serge.techno@gmail.com>
2024-07-31 00:22:08 +0300

    added Clerk Auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,14 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import Navbar from "./components/Navbar";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import { ClerkProvider } from '@clerk/nextjs';
+import Navbar from '@/components/Navbar';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Lama Dev Social Media App",
-  description: "Social media app built with Next.js",
+  title: 'Lama Dev Social Media App',
+  description: 'Social media app built with Next.js',
 };
 
 export default function RootLayout({
@@ -16,15 +17,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <div className="w-full bg-white px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
-          <Navbar />
-        </div>
-          <div className="bg-slate-100 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+    <ClerkProvider>
+      <html lang='en'>
+        <body className={inter.className}>
+          <div className='w-full bg-white px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64'>
+            <Navbar />
+          </div>
+          <div className='bg-slate-100 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64'>
             {children}
           </div>
         </body>
-    </html>
+      </html>
+    </ClerkProvider>
   );
 }
