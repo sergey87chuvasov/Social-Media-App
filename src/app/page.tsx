0f3789eb1c22e5f3ebe2db39af9af219ee4ae770
Sergey87 <serge.techno@gmail.com>
2024-08-01 23:31:12 +0300

    added User Stories Component Design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Stories from '@/components/Stories';
 
 const Homepage = () => {
   return (
-    <div className='flex gap-6'>
+    <div className='flex gap-6 pt-6'>
       <div className='hidden xl:block w-[20%]'>
         <LeftMenu />
       </div>
--- src/components/Stories.tsx
+++ src/components/Stories.tsx
@@ -1,5 +1,72 @@
+import Image from 'next/image';
+
 const Stories = () => {
-  return <div className=''></div>;
+  return (
+    <div className='p-4 bg-white rounded-lg shadow-md overflow-scroll text-xs scrollbar-hide'>
+      <div className='flex gap-8 w-max'>
+        <div className='flex flex-col items-center gap-2 cursor-pointer'>
+          <Image
+            src='https://images.pexels.com/photos/27330605/pexels-photo-27330605.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+            width={80}
+            height={80}
+            alt='img pic'
+            className='w-20 h-20 rounded-full ring-2'
+          />
+          <span className=''>Sergey</span>
+        </div>
+        <div className='flex flex-col items-center gap-2 cursor-pointer'>
+          <Image
+            src='https://images.pexels.com/photos/27330605/pexels-photo-27330605.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+            width={80}
+            height={80}
+            alt='img pic'
+            className='w-20 h-20 rounded-full ring-2'
+          />
+          <span className=''>Sergey</span>
+        </div>
+        <div className='flex flex-col items-center gap-2 cursor-pointer'>
+          <Image
+            src='https://images.pexels.com/photos/27330605/pexels-photo-27330605.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+            width={80}
+            height={80}
+            alt='img pic'
+            className='w-20 h-20 rounded-full ring-2'
+          />
+          <span className=''>Sergey</span>
+        </div>
+        <div className='flex flex-col items-center gap-2 cursor-pointer'>
+          <Image
+            src='https://images.pexels.com/photos/27330605/pexels-photo-27330605.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+            width={80}
+            height={80}
+            alt='img pic'
+            className='w-20 h-20 rounded-full ring-2'
+          />
+          <span className=''>Sergey</span>
+        </div>
+        <div className='flex flex-col items-center gap-2 cursor-pointer'>
+          <Image
+            src='https://images.pexels.com/photos/27330605/pexels-photo-27330605.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+            width={80}
+            height={80}
+            alt='img pic'
+            className='w-20 h-20 rounded-full ring-2'
+          />
+          <span className=''>Sergey</span>
+        </div>
+        <div className='flex flex-col items-center gap-2 cursor-pointer'>
+          <Image
+            src='https://images.pexels.com/photos/27330605/pexels-photo-27330605.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+            width={80}
+            height={80}
+            alt='img pic'
+            className='w-20 h-20 rounded-full ring-2'
+          />
+          <span className=''>Sergey</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Stories;
