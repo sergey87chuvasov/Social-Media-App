0809a77e43be7879e6653a405a735f54fc04ee0a
Sergey87 <serge.techno@gmail.com>
2024-08-23 00:03:40 +0300

    finished RightMenu Design

--- src/components/UserInfoCart.tsx
+++ src/components/UserInfoCart.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 const UserInfoCart = ({ userId }: { userId: string }) => {
@@ -8,6 +9,52 @@ const UserInfoCart = ({ userId }: { userId: string }) => {
         <Link className='text-blue-500 text-xs' href='/'>
           See All
         </Link>
+      </div>
+      <div className='flex flex-col gap-4 text-gray-500'>
+        <div className='flex items-center gap-2'>
+          <span className='text-xl text-black'>Lioyd Fleming</span>
+          <span className='text-sm'>@jonathan</span>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio
+          quidem, temporibus inventore accusantium
+        </p>
+        <div className='flex items-center gap-2'>
+          <Image src='/map.png' alt='img pic' width={16} height={16} />
+          <span>
+            Living in <b>Denver</b>
+          </span>
+        </div>
+        <div className='flex items-center gap-2'>
+          <Image src='/school.png' alt='img pic' width={16} height={16} />
+          <span>
+            Went to <b>Edgar High School</b>
+          </span>
+        </div>
+        <div className='flex items-center gap-2'>
+          <Image src='/work.png' alt='img pic' width={16} height={16} />
+          <span>
+            Works at <b>Apple Inc.</b>
+          </span>
+        </div>
+        <div className='flex items-center justify-between'>
+          <div className='flex gap-1 items-center'>
+            <Image src='/link.png' alt='icon pic' width={16} height={16} />
+            <Link href='#' className='text-blue-500 font-medium'>
+              serge.dev
+            </Link>
+          </div>
+          <div className='flex gap-1 items-center'>
+            <Image src='/date.png' alt='icon pic' width={16} height={16} />
+            <span>Joined November 2024</span>
+          </div>
+        </div>
+        <button className='bg-blue-500 text-white text-sm rounded-md p-2'>
+          Follow
+        </button>
+        <span className='text-red-400 self-end text-xs cursor-pointer'>
+          Block User
+        </span>
       </div>
     </div>
   );
--- src/components/UserMediaCart.tsx
+++ src/components/UserMediaCart.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 const UserMediaCart = ({ userId }: { userId: string }) => {
@@ -8,6 +9,72 @@ const UserMediaCart = ({ userId }: { userId: string }) => {
         <Link className='text-blue-500 text-xs' href='/'>
           See All
         </Link>
+      </div>
+      <div className='flex gap-4 justify-between flex-wrap'>
+        <div className='relative w-1/5 h-24'>
+          <Image
+            src='https://images.pexels.com/photos/11031989/pexels-photo-11031989.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+            alt='img pic'
+            fill
+            className='object-cover rounded-md'
+          />
+        </div>
+        <div className='relative w-1/5 h-24'>
+          <Image
+            src='https://images.pexels.com/photos/11031989/pexels-photo-11031989.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+            alt='img pic'
+            fill
+            className='object-cover rounded-md'
+          />
+        </div>
+        <div className='relative w-1/5 h-24'>
+          <Image
+            src='https://images.pexels.com/photos/11031989/pexels-photo-11031989.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+            alt='img pic'
+            fill
+            className='object-cover rounded-md'
+          />
+        </div>
+        <div className='relative w-1/5 h-24'>
+          <Image
+            src='https://images.pexels.com/photos/11031989/pexels-photo-11031989.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+            alt='img pic'
+            fill
+            className='object-cover rounded-md'
+          />
+        </div>
+        <div className='relative w-1/5 h-24'>
+          <Image
+            src='https://images.pexels.com/photos/11031989/pexels-photo-11031989.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+            alt='img pic'
+            fill
+            className='object-cover rounded-md'
+          />
+        </div>
+        <div className='relative w-1/5 h-24'>
+          <Image
+            src='https://images.pexels.com/photos/11031989/pexels-photo-11031989.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+            alt='img pic'
+            fill
+            className='object-cover rounded-md'
+          />
+        </div>
+        <div className='relative w-1/5 h-24'>
+          <Image
+            src='https://images.pexels.com/photos/11031989/pexels-photo-11031989.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+            alt='img pic'
+            fill
+            className='object-cover rounded-md'
+          />
+        </div>
+        <div className='relative w-1/5 h-24'>
+          <Image
+            src='https://images.pexels.com/photos/11031989/pexels-photo-11031989.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+            alt='img pic'
+            fill
+            className='object-cover rounded-md'
+          />
+        </div>
       </div>
     </div>
   );
